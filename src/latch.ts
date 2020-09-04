fc8497439ef5c6b0ae7d2f5cb38a487259da3814
Mikhail Nasyrov <streambot@gmail.com>
2020-09-04 15:01:28 +0700

    refactor: Refactored Latch to simple Latch and ValueLatch.

--- src/latch.ts
+++ src/latch.ts
@@ -1,21 +1,16 @@
-export class Latch<T> {
-  private promise: Promise<T>;
-  private resolver?: (value?: T | PromiseLike<T>) => void;
-  private rejector?: (reason?: any) => void;
+import {Mutex} from './mutex';
+
+export class Latch {
   private _isPending = true;
+  private mutex = Mutex.asLocked();
 
-  constructor(private count: number = 1) {
-    this.promise = new Promise<T>((resolver, rejector) => {
-      this.resolver = resolver;
-      this.rejector = rejector;
-    });
-  }
+  constructor(private count: number = 1) {}
 
-  static fromPromise<T>(source: PromiseLike<T>): Latch<T> {
-    const latch = new Latch<T>();
+  static fromPromise(source: PromiseLike<void>): Latch {
+    const latch = new Latch();
 
     source.then(
-      (value) => latch.release(value),
+      () => latch.release(),
       (reason) => latch.reject(reason),
     );
 
@@ -26,17 +21,12 @@ export class Latch<T> {
     return this._isPending;
   }
 
-  release(value: T | PromiseLike<T>) {
+  release() {
     if (this._isPending) {
       this.count--;
       if (this.count <= 0) {
         this._isPending = false;
-        this.promise = Promise.resolve<T>(value);
-        if (this.resolver) {
-          this.resolver(value);
-        }
-        this.resolver = undefined;
-        this.rejector = undefined;
+        this.mutex.release();
       }
     }
   }
@@ -44,15 +34,55 @@ export class Latch<T> {
   reject(reason?: any) {
     if (this._isPending) {
       this._isPending = false;
-      if (this.rejector) {
-        this.rejector(reason);
-      }
-      this.resolver = undefined;
-      this.rejector = undefined;
+      this.mutex.reject(reason);
     }
   }
 
-  wait(): Promise<T> {
-    return this.promise;
+  wait(): Promise<void> {
+    return this.mutex.wait();
+  }
+}
+
+export class ValueLatch<T> {
+  private valueRef: {value: T} | void = undefined;
+  private latch: Latch;
+
+  constructor(count?: number) {
+    this.latch = new Latch(count);
+  }
+
+  static fromPromise<T>(source: PromiseLike<T>): ValueLatch<T> {
+    const latch = new ValueLatch<T>();
+
+    source.then(
+      (value) => latch.release(value),
+      (reason) => latch.reject(reason),
+    );
+
+    return latch;
+  }
+
+  isPending(): boolean {
+    return this.latch.isPending();
+  }
+
+  release(value: T) {
+    this.valueRef = {value};
+    this.latch.release();
+  }
+
+  reject(reason?: any) {
+    this.latch.reject(reason);
+  }
+
+  async wait(): Promise<T> {
+    await this.latch.wait();
+
+    if (!this.valueRef) {
+      // Should never happens.
+      throw new Error('Value is not set');
+    }
+
+    return this.valueRef.value;
   }
 }
